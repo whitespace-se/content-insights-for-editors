e3edd840ac9cff1cd2ae89ed6d5494d5073001ac
Whitespace AB <dev@whitespace.se>
2020-06-29 15:35:34 +0200

    Add curl without ssl verification instead of file_get_contents

--- source/php/Util/Matomo.php
+++ source/php/Util/Matomo.php
@@ -147,7 +147,12 @@ class Matomo {
 
     $url = $this->matomoUrl . '?' . $queryString;
 
-    $result = file_get_contents($url);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
 
     return json_decode($result, true);
   }
